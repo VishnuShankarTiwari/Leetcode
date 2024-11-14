8988fac6a751e6178c2ffd80313dc9b423928296
Vishnu Shankar <143391134+Vishnu7250@users.noreply.github.com>
2024-11-15 02:31:02 +0530

    Time: 0 ms (100%), Space: 40.4 MB (39.69%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,16 +1,32 @@
-//RECURSION   RECURSION 
+//DP DP MEMOIZATION
+
+
 
 
 //if n <= 1, we return n
-// else recurse n-1 and n-2 in function and add them to form n th fibonacci and return in same way recursively
+//we take array dp with -1 at all element, if we check if dp[n] not equal to -1 then we return it 
+// otherwise we store the recursive value n-1 + n-1 at dp[n]
 
 class Solution {
+    int[] dp;
     public int fib(int n) {
         if(n <= 1) return n;
+        dp = new int[n+1];
+        Arrays.fill(dp, -1);
 
-        return fib(n-1) + fib(n-2);
+        return helper(n);
+    }
+
+    public int helper(int n){
+        if(n <= 1) return n;
+
+        if(dp[n] != -1) return dp[n];
+
+        dp[n] = helper(n-1) + helper(n-2);
+
+        return dp[n];
     }
 }
 
-// TC: (2^n)
+// TC: O(n)
 // SC: O(n)
