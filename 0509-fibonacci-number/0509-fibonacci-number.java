5da855791befde1c11079bc2d6211c0fcadf45a5
Vishnu Shankar <143391134+Vishnu7250@users.noreply.github.com>
2024-11-15 02:15:34 +0530

    Time: 8 ms (45.15%), Space: 40.4 MB (29.19%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,3 +1,9 @@
+//RECURSION   RECURSION 
+
+
+//if n <= 1, we return n
+// else recurse n-1 and n-2 in function and add them to form n th fibonacci and return in same way recursively
+
 class Solution {
     public int fib(int n) {
         if(n <= 1) return n;
@@ -5,3 +11,6 @@ class Solution {
         return fib(n-1) + fib(n-2);
     }
 }
+
+// TC: (2^n)
+// SC: O(n)
