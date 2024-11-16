fc99c7ea2eff64dc35a047a07565c33cac0021cb
Vishnu Shankar <143391134+Vishnu7250@users.noreply.github.com>
2024-11-16 23:04:12 +0530

    Time: 0 ms (100%), Space: 41.2 MB (17.47%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,41 +1,29 @@
-//DP DP  TABULATION TABULATION
+//DP DP  SPACE OPTIMIZATION
 
-//we cannot take adjacent elements so, we choose pick - nopick method to find the maximum sum
-// bottom up approach
-// use dp array to store the overlapping subproblems
-// we check if dp
-// use pick - nopick method and find the max between them
-// and store them in dp
-// return dp[n-1];
+// we take two variable prev2 and prev
+// iterate on array and always keep storing the last two index max sum
+// for next value prev2 = prev; prev = curr in same manner
+// return prev as the maxsum will be at n-1 index and prev will have max value
 
 
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        int[] dp = new int[n];
-        // Arrays.fill(dp, 0);
-        dp[0] = nums[0];
-        int neg = 0;
+        int prev = nums[0];
+        int prev2 = 0;
 
         for(int i=1; i<n; i++){
-            int pick = nums[i]; if(i>1) pick += dp[i-2];
-            int noPick = dp[i-1];
-            dp[i] = Math.max(pick, noPick);
+            int pick = nums[i]; if(i>1) pick += prev2;
+            int noPick = prev;
+            int curr = Math.max(pick, noPick);
+
+            prev2 = prev;
+            prev = curr;
         }
 
-        return dp[n-1];
+        return prev;
     }
-
-    // public int helper(int[] nums, int idx, int[] dp){
-    //     if(idx == 0) return nums[idx];
-    //     if(idx < 0) return 0;
-    //     if(dp[idx] != -1) return dp[idx];
-
-        
-
-    //     return dp[idx] = Math.max(pick, noPick);
-    // }
 }
 
 //TC: O(N)
-//SC: O(N)
+//SC: O(1)
