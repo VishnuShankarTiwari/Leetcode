536985565c8bde126e92c19718ec671b5e0ee7f9
Vishnu Shankar <143391134+Vishnu7250@users.noreply.github.com>
2024-11-16 23:02:23 +0530

    Time: 0 ms (100%), Space: 40.8 MB (77.7%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,20 +1,41 @@
+//DP DP  TABULATION TABULATION
+
+//we cannot take adjacent elements so, we choose pick - nopick method to find the maximum sum
+// bottom up approach
+// use dp array to store the overlapping subproblems
+// we check if dp
+// use pick - nopick method and find the max between them
+// and store them in dp
+// return dp[n-1];
+
+
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
         int[] dp = new int[n];
-        Arrays.fill(dp, -1);
+        // Arrays.fill(dp, 0);
+        dp[0] = nums[0];
+        int neg = 0;
 
-        return helper(nums, n-1, dp);
+        for(int i=1; i<n; i++){
+            int pick = nums[i]; if(i>1) pick += dp[i-2];
+            int noPick = dp[i-1];
+            dp[i] = Math.max(pick, noPick);
+        }
+
+        return dp[n-1];
     }
 
-    public int helper(int[] nums, int idx, int[] dp){
-        if(idx == 0) return nums[idx];
-        if(idx < 0) return 0;
-        if(dp[idx] != -1) return dp[idx];
+    // public int helper(int[] nums, int idx, int[] dp){
+    //     if(idx == 0) return nums[idx];
+    //     if(idx < 0) return 0;
+    //     if(dp[idx] != -1) return dp[idx];
 
-        int pick = nums[idx] + helper(nums, idx-2, dp);
-        int noPick = helper(nums, idx-1, dp);
+        
 
-        return dp[idx] = Math.max(pick, noPick);
-    }
+    //     return dp[idx] = Math.max(pick, noPick);
+    // }
 }
+
+//TC: O(N)
+//SC: O(N)
