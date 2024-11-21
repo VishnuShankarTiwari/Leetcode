686ce46ef3d826a31a60a691e7811c4d4ae06e3e
Vishnu Shankar <143391134+VishnuShankarTiwari@users.noreply.github.com>
2024-11-21 13:00:40 +0530

    Time: 1 ms (21.29%), Space: 41.5 MB (82.3%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,3 +1,9 @@
+// DP DP TABULATION TABULATION
+
+// we are using 2d dp array to store the previous subproblem ans
+//if grid is marked 1 we just simply return 0
+
+
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m = obstacleGrid.length;
@@ -9,21 +15,25 @@ class Solution {
         }
 
 
-        return helper(m-1, n-1, obstacleGrid,dp);
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+
+                if(obstacleGrid[i][j] == 1) dp[i][j] = 0;
+                else if(i == 0 && j == 0) dp[i][j] = 1;
+                else{
+                    int up = 0;
+                    int left = 0;
+                    if(i>0) up = dp[i-1][j];
+                    if(j>0) left = dp[i][j-1];
+                    dp[i][j] = up + left;
+                }
+            }
+        }
+
+
+        return dp[m-1][n-1];
     }
-        
-    public int helper(int i, int j, int[][] grid, int[][] dp){
-        if(i >= 0 && j >= 0 && grid[i][j] == 1) return 0;
-
-        if(i == 0 && j == 0) return 1;
-        if(i < 0 || j < 0) return 0;
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        int up = helper(i-1, j, grid, dp);
-        int left = helper(i, j-1, grid, dp);
-
-        return dp[i][j] = up + left;
-    }
-
 }
+
+// TC : O(N x M)
+// SC : (N x M)
