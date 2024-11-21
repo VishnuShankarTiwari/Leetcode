7a984081fe846461215c6df1bc6eb10dbcb18c56
Vishnu Shankar <143391134+VishnuShankarTiwari@users.noreply.github.com>
2024-11-21 13:00:56 +0530

    Time: 0 ms (100%), Space: 41.8 MB (30.92%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,7 +1,9 @@
-// DP DP TABULATION TABULATION
+// DP DP  STORAGE OPTIMIZATION
 
-// we are using 2d dp array to store the previous subproblem ans
+// we are using 1d dp array to store the previous subproblem ans as we only need previous row
+// use another temporary array  to store the same row and after evaluation thrn transfer them to prev after evaluation of the current row
 //if grid is marked 1 we just simply return 0
+// else if we reach the end return 1
 
 
 class Solution {
@@ -9,29 +11,26 @@ class Solution {
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
 
-        int[][] dp = new int[m][n];
-        for(int i=0; i<m; i++){
-            Arrays.fill(dp[i], -1);
-        }
-
+        int[] prev = new int[n];
+        Arrays.fill(prev, 0);
 
         for(int i=0; i<m; i++){
+            int[] temp = new int[n];
+            Arrays.fill(temp, 0);
+
             for(int j=0; j<n; j++){
-
-                if(obstacleGrid[i][j] == 1) dp[i][j] = 0;
-                else if(i == 0 && j == 0) dp[i][j] = 1;
+                if(obstacleGrid[i][j] == 1) temp[j] = 0;
+                else if(i == 0 && j == 0) temp[j] = 1;
                 else{
-                    int up = 0;
-                    int left = 0;
-                    if(i>0) up = dp[i-1][j];
-                    if(j>0) left = dp[i][j-1];
-                    dp[i][j] = up + left;
+                    int up = 0, left  = 0;
+                    if(i>0) up = prev[j];
+                    if(j>0) left = temp[j-1];
+                    temp[j] = up + left;
                 }
-            }
+            } 
+            prev = temp;
         }
-
-
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
 }
 
