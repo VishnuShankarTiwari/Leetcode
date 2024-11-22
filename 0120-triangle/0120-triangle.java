053ade3d2fd1130b4eefd02569825927f6dbca82
Vishnu Shankar <143391134+VishnuShankarTiwari@users.noreply.github.com>
2024-11-22 11:24:52 +0530

    Time: 1 ms (99.69%), Space: 43.7 MB (90.8%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,10 +1,10 @@
-//DP DP TABULATION TABULATION
+//DP DP MEMOIZATION MEMOIZATION
 
-//we take 2d dp array of size n x n
-// we check base case, last row with all columns
-//we try to start from n-1 to reach 0,0
-//we evaluate for down and diagonal condition 
-// take min of both, store in dp and return dp[0][0]
+//we take 2d dp array of size n x n with -1
+//we check dp[i][j] before calling the recursion
+//we try to start recursion from (0,0) and reach to last row(n-1) any column
+//we write recursion  for down and diagonal condition 
+// take min of both, store in dp and return
 
 
 class Solution {
@@ -12,23 +12,25 @@ class Solution {
         int n = triangle.size();
 
         int[][] dp = new int[n][n];
-
-        for(int j=0; j<n; j++){
-            dp[n-1][j] = triangle.get(n-1).get(j);
+        for(int i=0; i<n; i++){
+            Arrays.fill(dp[i], -1);
         }
 
-        for(int i=n-2; i>=0; i--){
-            for(int j=i; j>=0; j--){
-                int down = triangle.get(i).get(j) + dp[i+1][j];
-                int dia = triangle.get(i).get(j) + dp[i+1][j+1];
+        return helper(triangle, 0, 0, dp);
+    }
 
-                dp[i][j] = Math.min(down, dia);
-            }
-        }
+    public int helper(List<List<Integer>> tri, int i, int j, int[][] dp){
+        int n = tri.size();
+        if(i == n-1) return tri.get(i).get(j);
 
-        return dp[0][0];
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int down = tri.get(i).get(j) + helper(tri, i+1, j, dp);
+        int dia = tri.get(i).get(j) + helper(tri, i+1, j+1, dp);
+
+        return dp[i][j] = Math.min(down, dia);
     }
 }
 
 // TC : O(n x n)
-// SC : O(n x n)
+// SC : O(n x n) + O(n)
