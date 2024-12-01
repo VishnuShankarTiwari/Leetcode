dbcfe79ee0e406bafaf11a06eb98d38c0e405a0a
Vishnu Shankar <143391134+VishnuShankarTiwari@users.noreply.github.com>
2024-12-01 18:44:32 +0530

    Time: 3 ms (100%), Space: 77 MB (47.29%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -2,35 +2,25 @@ class Solution {
     public int[] rearrangeArray(int[] nums) {
         int n = nums.length;
 
-        //separate positive and negatives from nums
-        //create  positive and negative array
-        int[] pos = new int[n/2];
-        int[] neg = new int[n/2];
-        
-        //separation of pos & neg
-        int x = 0, y = 0;
-        for(int i : nums){
-            
-            if(i >= 0){
-                pos[x++] = i; 
+        //create a new array and initialize neg & pos idx
+        int[] arr = new int[n];
+        int posIdx = 0;
+        int negIdx = 1;
+
+        //store to new array in asked fashion & maintain neg, pos idx
+        for(int i=0; i<n; i++){
+            if(nums[i] < 0){
+                arr[negIdx] = nums[i];
+                negIdx += 2;
             }else{
-                neg[y++] = i;
+                arr[posIdx] = nums[i];
+                posIdx += 2;
             }
         }
 
-        //copy back positives to nums
-        for(int i=0; i<n/2; i++){
-            nums[i*2] = pos[i];
-        }
-
-        //copy back negatives to nums
-        for(int i=0; i<n/2; i++){
-            nums[2*i + 1] = neg[i];
-        }
-
-        return nums;
+        return arr;
     }
 }
 
-// TC : O(2n)
+// TC : O(n)
 // SC : O(n)
