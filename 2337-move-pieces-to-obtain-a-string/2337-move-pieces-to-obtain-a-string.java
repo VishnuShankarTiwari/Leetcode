e103b09889bc3fee96bd958d84bc70902b16316d
Vishnu Shankar <143391134+VishnuShankarTiwari@users.noreply.github.com>
2024-12-05 11:57:47 +0530

    Time: 15 ms (66.47%), Space: 45.7 MB (42.19%) - LeetHub

---
+++ 2337-move-pieces-to-obtain-a-string/2337-move-pieces-to-obtain-a-string.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean canChange(String start, String target) {
+        int n = start.length();
+        int i = 0;
+        int j = 0;
+        
+        while(i<n || j<n){
+            //skip all blank in start
+            while(i<n && start.charAt(i)=='_') i++;
+
+            //skip all blank in target
+            while(j<n && target.charAt(j)=='_') j++;
+
+            //count is same only if both the string end at same time
+            if(i==n || j==n) return (i==n && j==n);
+
+            //check false scenario
+            if(start.charAt(i) != target.charAt(j) ||
+            (start.charAt(i) == 'L' && j > i) || 
+            (start.charAt(i) == 'R' && j < i)
+            ){
+                return false;
+            }
+
+            i++;
+            j++;
+        }
+
+        return true;
+    }
+}
