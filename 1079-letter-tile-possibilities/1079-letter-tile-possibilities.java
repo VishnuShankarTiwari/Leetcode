b324470ccc09519c186f6419f558d3a577abc465
Vishnu Shankar <143391134+VishnuShankarTiwari@users.noreply.github.com>
2025-02-17 21:11:33 +0530

    Time: 2 ms (87.75%), Space: 41.3 MB (95.47%) - LeetHub

---
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.java
@@ -0,0 +1,27 @@
+class Solution {
+    int len;
+    public int numTilePossibilities(String tiles) {
+        len = tiles.length();
+        int freq[] = new int[26];
+        for(int i=0;i<len;i++){
+            freq[tiles.charAt(i) - 'A']++;
+        }
+        int count = backtrack(freq);
+        return count;
+    }
+
+    public int backtrack(int freq[]){
+        int count=0;
+        for(int i=0;i<26;i++){
+            if(freq[i]==0) continue;
+            count++;
+            freq[i]--;
+            count += backtrack(freq);
+            freq[i]++;
+        }
+        return count;
+    }
+}
+
+//TC : O(2^n)
+//SC : O(n)
